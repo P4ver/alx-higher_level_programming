8a057d1f0beb8c72c81f80bf49c5cbd8b4643ffb
P4ver <s.pales20@gmail.com>
2023-10-30 19:43:16 -0700

    the hard one

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+/**
+ * check_cycle -
+ * @list:
+ * Return: 0 if no cycle, 1 if there is a cycle,
+ **/
+int check_cycle(listint_t *list)
+{
+	listint_t *sl_w = list, *fs_t = list->next;
+
+	if (list == NULL || list->next == NULL)
+		return (0);
+	while (fs_t != NULL && fs_t->next != NULL)
+	{
+		if (sl_w == fs_t)
+			return (1);
+		sl_w = sl_w->next;
+		fs_t = fs_t->next->next;
+	}
+	return (0);
+}
