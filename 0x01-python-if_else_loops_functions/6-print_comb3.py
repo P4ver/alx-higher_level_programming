4781efdc6b3e00ea175655bcad31b83869c5ce85
P4ver <s.pales20@gmail.com>
2023-10-31 15:11:34 -0700

    task 6 ,pvr

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for n in range(0, 9):
-    for h in range (n + 1, 10):
+    for h in range(n + 1, 10):
         if n < 8:
             print("{}{}".format(n, h), end=", ")
         elif n == 8 and h == 9:
