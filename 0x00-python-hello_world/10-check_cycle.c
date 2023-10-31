2352fe6d7c933a66999cc73d1891d8a3f43d658b
P4ver <s.pales20@gmail.com>
2023-10-30 19:45:54 -0700

    the hard one

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -3,8 +3,9 @@
 #include <stdio.h>
 #include "lists.h"
 /**
- * check_cycle -
- * @list:
+ * check_cycle - function in C that checks if a
+ *	singly linked list has a cycle in it,
+ * @list: pntr to check,
  * Return: 0 if no cycle, 1 if there is a cycle,
  **/
 int check_cycle(listint_t *list)
