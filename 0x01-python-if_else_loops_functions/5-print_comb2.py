a9516befccd1bf1c5cfab85fc8ae7c9d6a919836
P4ver <s.pales20@gmail.com>
2023-10-31 13:40:35 -0700

    task 5pvr

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-for n in range(0, 99):
-    if n < 98:
+for n in range(0, 100):
+    if n < 99:
         print("{:02}".format(n), end=", ")
-    elif n == 98:
+    elif n == 99:
         print("{}".format(n))
