0b54cc7329d286f1e9f84631d69eb30ac7b505ae
P4ver <s.pales20@gmail.com>
2023-11-02 08:09:50 -0700

    task 0

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-if __name__ == '__main__':
-    import add_0
-    a = 1
-    b = 2
-    sum = add_0.add(a, b)
-    print(f"{1} + {2} = {3}".format(a, b, sum))
+from add_0 import add
+a = 1
+b = 2
+print(f"{1} + {2} = {3}".format(a, b, add(a, b)))
