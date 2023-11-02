28189a818da001379efe241babfd878225939429
P4ver <s.pales20@gmail.com>
2023-11-02 10:25:23 -0700

    taskpvr

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 if __name__ == '__main__':
     import sys
-    if len(sys.argv[1:]) == 0:
-        print("{} argument.".format(len(sys.argv[1:])))
+    if len(sys.argv) == 1:
+        print("{} argument.".format(len(sys.argv) - 1))
     else:
-        print("{} argument:".format(len(sys.argv[1:])))
-    for u in range(len(sys.argv[1:])):
+        print("{} argument:".format(len(sys.argv) - 1))
+    for u in range(len(sys.argv) - 1):
         print("{}: {}".format(u + 1 , sys.argv[u + 1]))
