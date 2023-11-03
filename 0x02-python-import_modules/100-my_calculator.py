91049daadfdfe62c842afbc29767710ba6c2e30d
P4ver <s.pales20@gmail.com>
2023-11-02 19:39:43 -0700

    adv

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -5,7 +5,7 @@ if __name__ == "__main__":
     len_ar = len(sys.argv)
     operator = ["+","-","*","/"]
     
-    if len_ar == 1:
+    if len_ar != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>\n")
         exit(1)
     elif sys.argv[2] not in operator:
@@ -15,11 +15,10 @@ if __name__ == "__main__":
         a_ar = int(sys.argv[1])
         b_ar = int(sys.argv[3])
         if sys.argv[2] == "+":
-            res = add(a_ar, b_ar)
+            print("{} + {} = {}".format(a_ar, b_ar, add(a_ar, b_ar)))
         elif sys.argv[2] == "-":
-            res = sub(a_ar, b_ar)
+            print("{} - {} = {}".format(a_ar, b_ar, sub(a_ar, b_ar)))
         elif sys.argv[2] == "*":
-            res = mul(a_ar, b_ar)
+            print("{} * {} = {}".format(a_ar, b_ar, mul(a_ar, b_ar)))
         elif sys.argv[2] == "/":
-            res = div(a_ar, b_ar)
-        print("{} {} {} = {}".format(a_ar, sys.argv[2], b_ar, res))
+            print("{} / {} = {}".format(a_ar, b_ar, div(a_ar, b_ar)))
