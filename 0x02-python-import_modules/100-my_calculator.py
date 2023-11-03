6eb0a5ce02e434724a3549e110c41a9614bd7380
P4ver <s.pales20@gmail.com>
2023-11-02 19:27:23 -0700

    adv adv

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -3,23 +3,23 @@ if __name__ == "__main__":
     import sys
     from calculator_1 import add, sub, mul, div
     len_ar = len(sys.argv)
-    if len_ar > 4 or len_ar < 4:
+    operator = ["+","-","*","/"]
+    
+    if len_ar == 1:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>\n")
         exit(1)
-    elif len_ar == 4:
-        ope = sys.argv[2]
+    elif sys.argv[2] not in operator:
+        print("Unknown operator. Available operators: +, -, * and /\n")
+        exit(1)
+    else:
         a_ar = int(sys.argv[1])
         b_ar = int(sys.argv[3])
-        res = 0
-        if ope == "+":
+        if sys.argv[2] == "+":
             res = add(a_ar, b_ar)
-        elif ope == "-":
+        elif sys.argv[2] == "-":
             res = sub(a_ar, b_ar)
-        elif ope == '*':
+        elif sys.argv[2] == "*":
             res = mul(a_ar, b_ar)
-        elif ope == "/":
+        elif sys.argv[2] == "/":
             res = div(a_ar, b_ar)
-        else:
-            print("Unknown operator. Available operators: +, -, * and /\n")
-            exit(1)
-        print("{} {} {} = {}".format(a_ar, ope, b_ar, res))
+        print("{} {} {} = {}".format(a_ar, sys.argv[2], b_ar, res))
