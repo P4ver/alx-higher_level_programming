decb901aa2f8f3c948dc30586d1c17f7032b24a0
P4ver <s.pales20@gmail.com>
2023-11-04 18:29:21 -0700

    task 6 pvr

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    for y in matrix:
-        u = 0
-        for k in y:
-            u = u + 1
-            print("{:d}".format(k), end="")
-            if u < 3:
-                print(" ", end="")
-        print("")
+    if matrix:        
+        for y in matrix:
+            u = 0
+            for k in y:
+                u = u + 1
+                print("{:d}".format(k), end="")
+                if u < 3:
+                    print(" ", end="")
+            print("")
