458e670c6a00d2cccc5e512d7b6e2a6f818222b9
P4ver <s.pales20@gmail.com>
2023-11-05 08:17:14 -0800

    10 task pvr

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+    nl_a = my_list + []
+    for u in range(len(my_list)):
+        if u % 2 == 0:
+            nl_a[u] = True
+        else:
+            nl_a[u] = False
+    return nl_a
