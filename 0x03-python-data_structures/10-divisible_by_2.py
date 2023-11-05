2e36992763e5bd3da2e38e85d552826d04ac46b1
P4ver <s.pales20@gmail.com>
2023-11-05 08:40:23 -0800

    10 task pvr

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -2,9 +2,11 @@
 def divisible_by_2(my_list=[]):
     if my_list:
         nl_a = my_list + []
-        for u in range(len(my_list)):
+        u = 0
+        while u < len(my_list):
             if u % 2 == 0:
                 nl_a[u] = True
             else:
                 nl_a[u] = False
+            u += 1
         return nl_a
