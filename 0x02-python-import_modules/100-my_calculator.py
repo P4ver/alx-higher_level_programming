b3ee7fda070b28b5cbfa8f1ab3c3bee26ef367d5
P4ver <s.pales20@gmail.com>
2023-11-06 17:02:51 -0800

    edit

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -3,7 +3,7 @@ if __name__ == "__main__":
     import sys
     from calculator_1 import add, sub, mul, div
     len_ar = len(sys.argv)
-    operator = ["+", "-", "*", "/"] 
+    operator = ["+", "-", "*", "/"]
     if len_ar != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
