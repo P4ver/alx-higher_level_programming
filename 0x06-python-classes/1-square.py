95b559ffafbc623b175355669d77e86dfcad9397
P4ver <s.pales20@gmail.com>
2023-11-22 14:13:22 -0800

    tsk pvr 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" square _mdl,"""
+
+
+class Square:
+    """class representing square."""
+
+    def __init__(self, size):
+        """Initializing new instance,
+
+        Args:
+            size(int): size of sqr,
+        """
+        self.__seze = size
