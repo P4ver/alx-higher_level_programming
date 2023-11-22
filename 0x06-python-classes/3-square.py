eabf24c88279978865dda78a5b2e2d6d7bc243da
P4ver <s.pales20@gmail.com>
2023-11-22 15:33:15 -0800

    tsk 3 pvr

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -16,10 +16,11 @@ class Square:
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
-    def area(self):
-        """Calc the area,
 
-        Return:
-            the area value,
+    def area(self):
+        """Calc the area.
+
+        Returns:
+            Area value,
         """
         return self.__size * self.__size
