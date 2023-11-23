69ea8c82e20a5c80fbf6e710f83906a907af7532
P4ver <s.pales20@gmail.com>
2023-11-22 16:34:47 -0800

    tsk pvr 4

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+"""Class square mdl."""
+
+
+class Square:
+    """Representing class square."""
+
+    def __init__(self, size=0):
+        """Initializing new instance.
+
+        Args:
+            size: size of sqr.
+        """
+        self.__size = size
+
+    @property
+    def size(self):
+        """Getter methode for size attr.
+
+        Returns:
+            the size square
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, vlr):
+        """Setter method for size attr.
+
+        Args:
+            vlr: the new size.
+        """
+        if type(vlr) != int:
+            raise TypeError("size must be an integer")
+        if vlr < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = vlr
+
+    def area(self):
+        """Calc square's area.
+
+        Returns:
+            The area vlr,
+        """
+        return self.__size * self.__size
