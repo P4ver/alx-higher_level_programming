8f887ecc201ff608ba9c0dd7dd4b5f4bc2fe4426
P4ver <s.pales20@gmail.com>
2023-11-27 15:09:14 -0800

    tsk 1 edit

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -6,14 +6,13 @@ Define class Rec.
 
 class Rectangle:
     """
-    Represent a rec.
+    Represent of a rec.
 
     Args:
         width: the width of rec.
         height: th height.
 
     """
-
     def __init__(self, width=0, height=0):
         """
         Initialize a new Rec.
@@ -26,8 +25,8 @@ class Rectangle:
             None
 
         """
-        self.__height = height
-        self.__width = width
+        self.height = height
+        self.width = width
 
     @property
     def width(self):
@@ -42,9 +41,9 @@ class Rectangle:
     @width.setter
     def width(self, value):
         """Set new val."""
-        if type(value) is not int or type(self.__width) is not int:
+        if type(value) is not int:
             raise TypeError("width must be an integer")
-        if value < 0 or self.__width < 0:
+        if value < 0:
             raise ValueError("width must be >= 0")
         self.__width = value
 
@@ -61,8 +60,8 @@ class Rectangle:
     @height.setter
     def height(self, value):
         """Set new val."""
-        if type(value) is not int or type(self.__height) is not int:
+        if type(value) is not int:
             raise TypeError("height must be an integer")
-        if value < 0 or self.__height < 0:
+        if value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
