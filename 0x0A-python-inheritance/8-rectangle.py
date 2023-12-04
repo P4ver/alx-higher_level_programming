386845b2132f78414367fc564b84e2af471a2c37
P4ver <s.pales20@gmail.com>
2023-12-04 14:45:22 -0800

    tsk edit 8

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,19 +1,6 @@
 #!/usr/bin/python3
 """geo module,"""
-
-
-class BaseGeometry:
-    """geo class."""
-    def area(self):
-        """display exceptation."""
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """funct that validate vlue."""
-        if not type(value) is int:
-            raise TypeError("{} must be an integer".format(name))
-        if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
