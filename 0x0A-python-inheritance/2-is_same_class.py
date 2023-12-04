0e288f0acefe8d572661ef5d4ec902919d720c9f
P4ver <s.pales20@gmail.com>
2023-12-04 11:53:07 -0800

    tsk pvr 2

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""is same object module."""
+
+
+def is_same_class(obj, a_class):
+    """method returns true or false if obj is instance of class."""
+    return type(obj) is a_class
