4e509159c51cb9606f8215d589d04ebd818fdf54
P4ver <s.pales20@gmail.com>
2023-12-04 12:00:23 -0800

    tsk pvr 3

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""module is_kind_of_class."""
+
+
+def is_kind_of_class(obj, a_class):
+    """returns true if obj isinstance or class that inherited from."""
+    return isinstance(obj, a_class)
