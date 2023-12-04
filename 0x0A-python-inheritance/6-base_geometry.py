75576427557f9d073ebeb18f6d92dd1e6d0a2d49
P4ver <s.pales20@gmail.com>
2023-12-04 13:18:11 -0800

    tsk pvr

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""geo module,"""
+
+
+class BaseGeometry:
+    """empty class."""
+    def area(self):
+        """display exceptation."""
+        raise Exception("area() is not implemented")
