9ee2aa4f3ee801b9f7c5a538ee0629580f41cb13
P4ver <s.pales20@gmail.com>
2023-12-04 12:24:06 -0800

    tsk pvr 4

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -6,3 +6,5 @@ def inherits_from(obj, a_class):
     """if the object is an instance of a class that inherited,return true."""
     if not type(obj) is a_class:
         return isinstance(obj, a_class)
+    else:
+        return False
