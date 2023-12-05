16d36634099f3a8c0a27205463030891e87d3e3f
P4ver <s.pales20@gmail.com>
2023-12-05 09:02:11 -0800

    tsk 0 pvr

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -6,4 +6,4 @@ def read_file(filename=""):
     """function reads fl."""
     with open("my_file_0.txt", "r") as fl_nm:
         rd_fl = fl_nm.read()
-        print(rd_fl.strip())
+    print(rd_fl.strip())
