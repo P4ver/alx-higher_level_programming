52673e2547fff6bb20dc2b22ab2a926330ec9670
P4ver <s.pales20@gmail.com>
2023-12-10 15:03:23 -0800

    tsk pvr

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -9,7 +9,6 @@ class Square(Rectangle):
     def __init__(self, size, x=0, y=0, id=None):
         """Initializing."""
         super().__init__(size, size, x, y, id)
-        self.size = size
 
     @property
     def size(self):
@@ -25,3 +24,23 @@ class Square(Rectangle):
     def __str__(self):
         """str representation for sqr."""
         return f"[{type(self).__name__}] ({self.id}) {super().x}/{super().y} - {self.width}"
+
+    def update(self, *args, **kwargs):
+        if args:
+            if len(args) == 2:
+                self.size = args[1]
+            if len(args) == 3:
+                self.x = args[2]
+            if len(args) == 4:
+                self.y = args[3]
+            else:
+                self.id = args[0]
+        if kwargs:
+            if 'id' in kwargs:
+                self.id = kwargs['id']
+            if 'size' in kwargs:
+                self.size = kwargs['size']
+            if 'x' in kwargs:
+                self.x = kwargs['x']
+            if 'y' in kwargs:
+                self.y = kwargs['y']
