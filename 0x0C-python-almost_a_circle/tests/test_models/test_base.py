3d0fae99924f5c5d18a3c7f07958e0f2a0db4753
P4ver <s.pales20@gmail.com>
2023-12-11 12:29:19 -0800

    tsd

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -6,6 +6,9 @@ from models.square import Square
 
 
 class TestBase(unittest.TestCase):
+    def setUp(self):
+        Base._Base__nb_objects = 0
+
     def test_base_crt(self):
         pv = Base()
         self.assertIsInstance(pv, Base)
