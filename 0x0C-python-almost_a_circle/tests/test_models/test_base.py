43a1c3b2e65199490ca4e55f56e67625ad8c9544
P4ver <s.pales20@gmail.com>
2023-12-11 12:23:14 -0800

    tsk edit

---
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+import unittest
+from models.base import Base
+from models.rectangle import Rectangle
+from models.square import Square
+
+
+class TestBase(unittest.TestCase):
+    def test_base_crt(self):
+        pv = Base()
+        self.assertIsInstance(pv, Base)
+        self.assertIsNotNone(pv.id)
+
+    def test_chk(self):
+        pv = Base(135)
+        self.assertEqual(pv.id, 135)
+
+
+if __name__ == '__main__':
+    unittest.main()
