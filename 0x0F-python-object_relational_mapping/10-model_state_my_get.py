7e2e631ecb31eae8244db84b9ba2332af897aba1
P4ver <s.pales20@gmail.com>
2024-02-18 16:13:04 -0800

    tsk

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """
-Script lst all State obj that contain the letter from db SQLAlchemy
+Script prints the stat obj with the name passed as arg
+from db SQLAlchemy
 """
 
 from model_state import Base, State
@@ -9,17 +10,16 @@ from sqlalchemy.orm import sessionmaker
 from sys import argv
 
 if __name__ == "__main__":
-    # create an engine
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
         argv[1], argv[2], argv[3]), pool_pre_ping=True)
-    # create a configured "Session" class
     Session = sessionmaker(bind=engine)
-    # create a Session
     session = Session()
     Base.metadata.create_all(engine)
 
-    st = session.query(State).filter(State.name.like('%a%'))\
-                                 .order_by(State.id).all()
-    for se in st:
-        print("{}: {}".format(se.id, se.name))
+    s = session.query(State).filter(State.name == argv[4]).first()
+
+    if s:
+        print("{}".format(s.id))
+    else:
+        print("Not found")
     session.close()
