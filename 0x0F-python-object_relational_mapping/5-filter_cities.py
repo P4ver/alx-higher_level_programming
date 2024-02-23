4a27ce840d6e8692a9f2b05a775037cadeb6bb95
P4ver <s.pales20@gmail.com>
2024-02-23 12:43:23 -0800

    tsk

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -11,23 +11,27 @@ if __name__ == "__main__":
     db_name = sys.argv[3]
     state_name = sys.argv[4]
 
-    db = MySQLdb.connect(host="localhost", port=3306, user=mysql_username,
-                         passwd=mysql_password, db=db_name)
+    try:
+        db = MySQLdb.connect(host="localhost", port=3306, user=mysql_username,
+                             passwd=mysql_password, db=db_name)
+        cursor = db.cursor()
 
-    cursor = db.cursor()
+        cursor.execute(
+            "SELECT GROUP_CONCAT(cities.name SEPARATOR ', ') FROM cities \
+            JOIN states ON cities.state_id = states.id \
+            WHERE states.name = %s ORDER BY cities.id ASC", (state_name,)
+        )
 
-    cursor.execute(
-        "SELECT GROUP_CONCAT(cities.name SEPARATOR ', ') FROM cities \
-        JOIN states ON cities.state_id = states.id \
-        WHERE states.name = %s ORDER BY cities.id ASC", (state_name,)
-    )
+        res = cursor.fetchone()
 
-    res = cursor.fetchone()
+        if res and res[0]:
+            print(res[0])
+        else:
+            print("No cities found for the specified state")
 
-    if res:
-        print(res[0])
-    else:
-        print("No cities found for the specified state")
+    except Exception as e:
+        print("An error occurred:", e)
 
-    cursor.close()
-    db.close()
+    finally:
+        cursor.close()
+        db.close()
